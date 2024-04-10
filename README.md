c07274ed0a4fa66de5b8c4ebe8b85e5de947a4fe
Matt Simerson <matt@tnpi.net>
2024-04-09 17:58:47 -0700

    import from haraka/Haraka

--- README.md
+++ README.md
@@ -3,55 +3,14 @@
 
 [![NPM][npm-img]][npm-url]
 
-# haraka-plugin-template
-
-Clone me, to create a new Haraka plugin!
-
-# Template Instructions
-
-These instructions will not self-destruct after use. Use and destroy.
-
-See also, [How to Write a Plugin](https://github.com/haraka/Haraka/wiki/Write-a-Plugin) and [Plugins.md](https://github.com/haraka/Haraka/blob/master/docs/Plugins.md) for additional plugin writing information.
-
-## Create a new repo for your plugin
-
-Haraka plugins are named like `haraka-plugin-something`. All the namespace after `haraka-plugin-` is yours for the taking. Please check the [Plugins](https://github.com/haraka/Haraka/blob/master/Plugins.md) page and a Google search to see what plugins already exist.
-
-Once you've settled on a name, create the GitHub repo. On the repo's main page, click the _Clone or download_ button and copy the URL. Then paste that URL into a local ENV variable with a command like this:
-
-```sh
-export MY_GITHUB_ORG=haraka
-export MY_PLUGIN_NAME=haraka-plugin-SOMETHING
-```
-
-Clone and rename the template repo:
-
-```sh
-git clone git@github.com:haraka/haraka-plugin-template.git
-mv haraka-plugin-template $MY_PLUGIN_NAME
-cd $MY_PLUGIN_NAME
-git remote rm origin
-git remote add origin "git@github.com:$MY_GITHUB_ORG/$MY_PLUGIN_NAME.git"
-```
-
-Now you'll have a local git repo to begin authoring your plugin
-
-## rename boilerplate
-
-Replaces all uses of the word `template` with your plugin's name.
-
-./redress.sh [something]
-
-You'll then be prompted to update package.json and then force push this repo onto the GitHub repo you've created earlier.
-
-# Add your content here
+# haraka-plugin-dkim
 
 ## INSTALL
 
 ```sh
 cd /path/to/local/haraka
-npm install haraka-plugin-template
-echo "template" >> config/plugins
+npm install haraka-plugin-dkim
+echo "dkim" >> config/plugins
 service haraka restart
 ```
 
@@ -60,17 +19,136 @@ service haraka restart
 If the default configuration is not sufficient, copy the config file from the distribution into your haraka config dir and then modify it:
 
 ```sh
-cp node_modules/haraka-plugin-template/config/template.ini config/template.ini
-$EDITOR config/template.ini
+cp node_modules/haraka-plugin-dkim/config/dkim.ini config/dkim.ini
+$EDITOR config/dkim.ini
 ```
 
-## USAGE
+## SIGNING
+
+This plugin implements the [DKIM Core specification](dkimcore.org).
+
+### Getting Started
+
+Generate a DKIM selector and keys for your domain:
+
+```sh
+cd /path/to/haraka/config/dkim
+./dkim_key_gen.sh example.org
+```
+
+Within the config/dkim/${domain} directory will be 4 files:
+
+```sh
+ls config/dkim/example.org/
+dns private public selector
+```
+
+The selector file contains the DNS label where the DKIM public key is published. The `private` and `public` files contain the DKIM keys.
+
+The `dns` file contains a formatted record of the public key suitable for copy/pasting into your domains zone file. It also has suggestions for DKIM, SPF, and DMARC policy records.
+
+The DKIM DNS record will look like this:
+
+    may2013._domainkey TXT "v=DKIM1;p=[public key stripped of whitespace];"
+
+The values in the address have the following meaning:
+
+    hash: h=[ sha1 | sha256 ]
+    test; t=[ s | s:y ]
+    granularity: g=[ ]
+    notes: n=[ ]
+    services: s=[email]
+    keytypes: [ rsa ]
+
+## Key size
+
+The default key size created by `dkim_key_gen.sh` is 2048. That is considered secure as of mid-2024.
+
+# What to sign
+
+The DKIM signing key for messages from example.org _should_ be signed with
+a DKIM key for example.org. Failing to do so will result in messages not
+having an _aligned_ DKIM signature. For DMARC enabled domains, this will
+likely result in deliverability problems.
+
+For correct alignment, Haraka signs each message with that domains DKIM key.
+For an alternative, see the legacy Single Domain Configuration below.
+
+# Configuration
+
+DKIM signing is configured in the sign section of `dkim.ini`.
+
+```ini
+[sign]
+enabled = [ 1 | true | yes ], default=false
+headers = list, of; headers (REQUIRED)
+
+; for single domain configuration
+selector = name
+domain = name
+```
+
+* headers: the list of headers that should be signed, separated by commas, colons or semi-colons. Signing prevents tampering with the specified headers. The 'From' header is required by the RFC and will be added if missing.
+
+## Single Domain Configuration
+
+To sign all messages with a single DKIM key, you must set the selector and domain in dkim.ini. You must also save your DKIM private key in the file `dkim.private.key` in the Haraka config directory.
+
+- selector - Set this to the selector name published in DNS under the \_domainkey sub-domain of the domain referenced below.
+- domain - Set this to the domain name that will be used to sign messages which don't match a per-domain DKIM key. The DNS TXT entry for:
+
+    <selector>.\_domainkey.<domain>
+
+Test that your DKIM key is published properly with a DNS request like this:
+
+```sh
+drill TXT $SELECTOR._domainkey.$DOMAIN
+dig TXT $SELECTOR._domainkey.$DOMAIN +short
+```
+
+### Example DNS query
+
+```sh
+export SELECTOR=mar2013
+export DOMAIN=simerson.net
+$ dig TXT $SELECTOR._domainkey.$DOMAIN +short
+"v=DKIM1;p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoyUzGOTSOmakY8BcxXgi0mN/nFegLBPs7aaGQUtjHfa8yUrt9T2j6GSXgdjLuG3R43WjePQv3RHzc+bwwOkdw0XDOXiztn5mhrlaflbVr5PMSTrv64/cpFQKLtgQx8Vgqp7Dh3jw13rLomRTqJFgMrMHdhIibZEa69gtuAfDqoeXo6QDSGk5JuBAeRHEH27FriHulg5ob" "4F4lmh7fMFVsDGkQEF6jaIVYqvRjDyyQed3R3aTJX3fpb3QrtRqvfn/LAf+3kzW58AjsERpsNCSTD2RquxbnyoR/1wdGKb8cUlD/EXvqtvpVnOzHeSeMEqex3kQI8HOGsEehWZlKd+GqwIDAQAB"
+```
+
+# DKIM VERIFY
+
+Verify DKIM signatures as defined by RFC 6376 and add an Authentication-Results header as appropriate.
+
+## Configuration
+
+```ini
+[verify]
+; allowed_time_skew = (How far can we stretch on time matching, in secs. Useful when clock is skewed.)
+; sigerror_log_level =
+```
+
+## Testing
+
+This plugin provides a command-line test tool that can be used to
+debug DKIM issues or to check results.
+
+```
+# dkimverify < message
+identity="@gmail.com" domain="gmail.com" result=pass
+```
+
+You can add `--debug` to the option arguments to see a full trace of the processing.
+
+## Notes
+
+This plugin and underlying library do not currently support DKIM body length limits (l=).
+
 
 <!-- leave these buried at the bottom of the document -->
 
-[ci-img]: https://github.com/haraka/haraka-plugin-template/actions/workflows/ci.yml/badge.svg
-[ci-url]: https://github.com/haraka/haraka-plugin-template/actions/workflows/ci.yml
-[clim-img]: https://codeclimate.com/github/haraka/haraka-plugin-template/badges/gpa.svg
-[clim-url]: https://codeclimate.com/github/haraka/haraka-plugin-template
-[npm-img]: https://nodei.co/npm/haraka-plugin-template.png
-[npm-url]: https://www.npmjs.com/package/haraka-plugin-template
+[ci-img]: https://github.com/haraka/haraka-plugin-dkim/actions/workflows/ci.yml/badge.svg
+[ci-url]: https://github.com/haraka/haraka-plugin-dkim/actions/workflows/ci.yml
+[clim-img]: https://codeclimate.com/github/haraka/haraka-plugin-dkim/badges/gpa.svg
+[clim-url]: https://codeclimate.com/github/haraka/haraka-plugin-dkim
+[npm-img]: https://nodei.co/npm/haraka-plugin-dkim.png
+[npm-url]: https://www.npmjs.com/package/haraka-plugin-dkim
--- index.js
+++ index.js
@@ -1,24 +1,349 @@
-"use strict";
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const async = require('async')
+const addrparser = require('address-rfc2822')
+
+const dkim = require('./lib/dkim')
+
+const { DKIMVerifyStream, DKIMSignStream } = dkim
 
 exports.register = function () {
-  this.load_template_ini();
+  this.load_dkim_ini()
 
-  // register hooks here. More info at https://haraka.github.io/core/Plugins/
-  // this.register_hook('data_post', 'do_stuff_with_message')
-};
+  this.register_hook('data_post', 'dkim_verify')
+  this.register_hook('queue_outbound', 'hook_pre_send_trans_email')
+}
 
-exports.load_template_ini = function () {
+exports.load_dkim_ini = function () {
+
   this.cfg = this.config.get(
-    "template.ini",
+    'dkim.ini',
     {
-      booleans: [
-        "+enabled", // this.cfg.main.enabled=true
-        "-disabled", // this.cfg.main.disabled=false
-        "+feature_section.yes", // this.cfg.feature_section.yes=true
-      ],
+      booleans: ['-sign.enabled'],
     },
     () => {
-      this.load_example_ini();
+      this.load_dkim_ini()
     },
-  );
-};
+  )
+
+  if (this.cfg.verify === undefined) this.cfg.verify = {}
+  if (!this.cfg.verify.timeout) {
+    this.cfg.verify.timeout = this.timeout ? this.timeout - 1 : 29
+  }
+
+  this.load_dkim_default_key()
+  this.cfg.headers_to_sign = this.get_headers_to_sign()
+}
+
+// dkim_signer
+// Implements DKIM core as per www.dkimcore.org
+
+exports.load_dkim_default_key = function () {
+  this.private_key = this.config
+    .get('dkim.private.key', 'data', () => {
+      this.load_dkim_default_key()
+    })
+    .join('\n')
+}
+
+exports.load_key = function (file) {
+  return this.config.get(file, 'data').join('\n')
+}
+
+exports.hook_pre_send_trans_email = function (next, connection) {
+  if (!this.cfg.sign.enabled) return next()
+  if (!connection?.transaction) return next()
+
+  if (connection.transaction.notes?.dkim_signed) {
+    connection.logdebug(this, 'already signed')
+    return next()
+  }
+
+  exports.get_sign_properties(connection, (err, props) => {
+    if (!connection?.transaction) return next()
+    // props: selector, domain, & private_key
+    if (err) connection.logerror(this, `${err.message}`)
+
+    if (!this.has_key_data(connection, props)) return next()
+
+    connection.logdebug(this, `domain: ${props.domain}`)
+
+    const txn = connection.transaction
+    props.headers = this.cfg.headers_to_sign
+
+    txn.message_stream.pipe(
+      new DKIMSignStream(props, txn.header, (err2, dkim_header) => {
+        if (err2) {
+          txn.results.add(this, { err: err2.message })
+          return next(err2)
+        }
+
+        connection.loginfo(this, `signed for ${props.domain}`)
+        txn.results.add(this, { pass: dkim_header })
+        txn.add_header('DKIM-Signature', dkim_header)
+
+        connection.transaction.notes.dkim_signed = true
+        next()
+      }),
+    )
+  })
+}
+
+exports.get_sign_properties = function (connection, done) {
+  if (!connection.transaction) return
+
+  const domain = this.get_sender_domain(connection)
+
+  if (!domain) {
+    connection.transaction.results.add(this, {
+      msg: 'sending domain not detected',
+      emit: true,
+    })
+  }
+
+  const props = { domain }
+
+  this.get_key_dir(connection, props, (err, keydir) => {
+    if (err) {
+      console.error(`err: ${err}`)
+      connection.logerror(this, err)
+      return done(
+        new Error(`Error getting DKIM key_dir for ${domain}: ${err}`),
+        props,
+      )
+    }
+
+    if (!connection.transaction) return done(null, props)
+
+    // a directory for ${domain} exists
+    if (keydir) {
+      props.domain = path.basename(keydir) // keydir might be apex (vs sub)domain
+      props.private_key = this.load_key(
+        path.join('dkim', props.domain, 'private'),
+      )
+      props.selector = this.load_key(
+        path.join('dkim', props.domain, 'selector'),
+      ).trim()
+
+      if (!props.selector) {
+        connection.transaction.results.add(this, {
+          err: `missing selector for domain ${domain}`,
+        })
+      }
+      if (!props.private_key) {
+        connection.transaction.results.add(this, {
+          err: `missing dkim private_key for domain ${domain}`,
+        })
+      }
+
+      if (props.selector && props.private_key) {
+        // AND has correct files
+        return done(null, props)
+      }
+    }
+
+    // try [default / single domain] configuration
+    if (this.cfg.sign.domain && this.cfg.sign.selector && this.private_key) {
+      connection.transaction.results.add(this, {
+        msg: 'using default key',
+        emit: true,
+      })
+
+      props.domain = this.cfg.sign.domain
+      props.private_key = this.private_key
+      props.selector = this.cfg.sign.selector
+
+      return done(null, props)
+    }
+
+    console.error(`no valid DKIM properties found`)
+    done(null, props)
+  })
+}
+
+exports.get_key_dir = function (connection, props, done) {
+  if (!props.domain) return done()
+
+  // split the domain name into labels
+  const labels = props.domain.split('.')
+  const haraka_dir = process.env.HARAKA || ''
+
+  // list possible matches (ex: mail.example.com, example.com, com)
+  const dom_hier = []
+  for (let i = 0; i < labels.length; i++) {
+    const dom = labels.slice(i).join('.')
+    dom_hier[i] = path.resolve(haraka_dir, 'config', 'dkim', dom)
+  }
+
+  async.detectSeries(
+    dom_hier,
+    (filePath, iterDone) => {
+      fs.stat(filePath, (err, stats) => {
+        if (err) return iterDone(null, false)
+        iterDone(null, stats.isDirectory())
+      })
+    },
+    (err, results) => {
+      connection.logdebug(this, results)
+      done(err, results)
+    },
+  )
+}
+
+exports.has_key_data = function (conn, props) {
+  let missing = undefined
+
+  // Make sure we have all the relevant configuration
+  if (!props.private_key) {
+    missing = 'private key'
+  } else if (!props.selector) {
+    missing = 'selector'
+  } else if (!props.domain) {
+    missing = 'domain'
+  }
+
+  if (missing) {
+    if (props.domain) {
+      conn.lognotice(this, `skipped: no ${missing} for ${props.domain}`)
+    } else {
+      conn.lognotice(this, `skipped: no ${missing}`)
+    }
+    return false
+  }
+
+  conn.logprotocol(
+    this,
+    `using selector: ${props.selector} at domain ${props.domain}`,
+  )
+  return true
+}
+
+exports.get_headers_to_sign = function () {
+  if (!this.cfg?.sign?.headers) return ['from']
+
+  const headers = this.cfg.sign.headers
+    .toLowerCase()
+    .replace(/\s+/g, '')
+    .split(/[,;:]/)
+
+  // From MUST be present
+  if (!headers.includes('from')) headers.push('from')
+
+  return headers
+}
+
+exports.get_sender_domain = function (connection) {
+  const txn = connection?.transaction
+  if (!txn) return
+
+  // fallback: use Envelope FROM when header parsing fails
+  let domain
+  if (txn.mail_from.host) {
+    try {
+      domain = txn.mail_from.host.toLowerCase()
+    } catch (e) {
+      connection.logerror(this, e)
+    }
+  }
+
+  // In case of forwarding, only use the Envelope
+  if (txn.notes.forward) return domain
+  if (!txn.header) return domain
+
+  // the DKIM signing key should be aligned with the domain in the From
+  // header (see DMARC). Try to parse the domain from there.
+  const from_hdr = txn.header.get_decoded('From')
+  if (!from_hdr) return domain
+
+  // The From header can contain multiple addresses and should be
+  // parsed as described in RFC 2822 3.6.2.
+  let addrs
+  try {
+    addrs = addrparser.parse(from_hdr)
+  } catch (e) {
+    connection.logerror(
+      this,
+      `address-rfc2822 failed to parse From header: ${from_hdr}`,
+    )
+    return domain
+  }
+  if (!addrs || !addrs.length) return domain
+
+  // If From has a single address, we're done
+  if (addrs.length === 1 && addrs[0].host) {
+    let fromHost = addrs[0].host()
+    if (fromHost) {
+      // don't attempt to lower a null or undefined value #1575
+      fromHost = fromHost.toLowerCase()
+    }
+    return fromHost
+  }
+
+  // If From has multiple-addresses, we must parse and
+  // use the domain in the Sender header.
+  const sender = txn.header.get_decoded('Sender')
+  if (sender) {
+    try {
+      domain = addrparser.parse(sender)[0].host().toLowerCase()
+    } catch (e) {
+      connection.logerror(this, e)
+    }
+  }
+  return domain
+}
+
+dkim.DKIMObject.prototype.debug = (str) => {
+  exports.logdebug(str)
+}
+
+DKIMVerifyStream.prototype.debug = (str) => {
+  exports.logdebug(str)
+}
+
+exports.dkim_verify = function (next, connection) {
+  const txn = connection?.transaction
+  if (!txn) return next()
+
+  const verifier = new DKIMVerifyStream(this.cfg.verify, (err, result, results) => {
+    if (err) {
+      txn.results.add(this, { err })
+      return next()
+    }
+    if (!results || results.length === 0) {
+      txn.results.add(this, { skip: 'no/bad dkim signature' })
+      return next(CONT, 'no/bad signature')
+    }
+    results.forEach((res) => {
+      let res_err = ''
+      if (res.error) res_err = ` (${res.error})`
+      connection.auth_results(
+        `dkim=${res.result}${res_err} header.i=${res.identity} header.d=${res.domain} header.s=${res.selector}`,
+      )
+      connection.loginfo(
+        this,
+        `identity="${res.identity}" domain="${res.domain}" selector="${res.selector}" result=${res.result} ${res_err}`,
+      )
+
+      // save to ResultStore
+      const rs_obj = JSON.parse(JSON.stringify(res))
+      if (res.result === 'pass') {
+        rs_obj.pass = res.domain
+      } else if (res.result === 'fail') {
+        rs_obj.fail = res.domain + res_err
+      } else {
+        rs_obj.err = res.domain + res_err
+      }
+      txn.results.add(this, rs_obj)
+    })
+
+    connection.logdebug(this, JSON.stringify(results))
+    // Store results for other plugins
+    txn.notes.dkim_results = results
+    next()
+  })
+
+  txn.message_stream.pipe(verifier, { line_endings: '\r\n' })
+}
